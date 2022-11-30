712900daab43d17739678af3069a18a8ab33ebe2
Alan62 <beaucheron.alan@free.fr>
2022-11-30 13:19:31 +0100

    template modal

---
+++ src/lib/components/Modal.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './StyleModal.css'
+
+const Modal = ({ isShowing, hide, text }) =>
+  isShowing
+    ? ReactDOM.createPortal(
+        <>
+          <div className="modalOverlay">
+            <div className="modalOverlay__wrapper">
+              <div className="modalOverlay__wrapper__header">
+                <h4>{text}</h4>
+                <div
+                  className="modalOverlay__wrapper__header__closeButton"
+                  onClick={hide}
+                >
+                  &times;
+                </div>
+              </div>
+            </div>
+          </div>
+        </>,
+        document.getElementById('modal-root')
+      )
+    : null
+
+export default Modal
